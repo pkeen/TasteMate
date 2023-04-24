482c3f2f136f13f85f1f8652ac751959e05c3f49
pete <pkeen7@gmail.com>
2023-04-24 08:37:38 -0600

    add hardcoded Business component

---
+++ src/components/Business.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Business() {
+
+    const business = {
+        imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+        name: 'MarginOtto Pizzeria',
+        address: '1010 Paddington Way',
+        city: 'Flavortown',
+        state: 'NY',
+        zipCode: '10101',
+        category: 'Italian',
+        rating: 4.5,
+        reviewCount: 90 
+    }
+    return (
+        <div>
+            <img src={business.imageSrc} />
+            <h2>{business.name}</h2>
+            <p>{business.address}</p>
+            <p>{business.city}</p>
+            <p>{business.state}</p>
+            <p>{business.zipCode}</p>
+            <p>{business.category}</p>
+            <p>{business.rating}</p>
+            <p>{business.reviewCount}</p>
+        </div>
+    )
+}
