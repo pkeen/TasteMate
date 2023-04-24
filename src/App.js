a4468893aaa8d072725b8f20553bb799f59adb4e
pete <pkeen7@gmail.com>
2023-04-24 11:51:31 -0600

    add SearchBar to main App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import BusinessList from './components/BusinessList';
-
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div className="App">
-
+      <SearchBar />
       <BusinessList />
     </div>
 
