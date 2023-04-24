ba4c71bcf085cbee5753004a98f53cb720e978b2
pete <pkeen7@gmail.com>
2023-04-24 08:51:04 -0600

    add BusinessList component

---
+++ src/components/BusinessList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Business from "./Business";
+
+function BusinessList() {
+
+    const businessList = [];
+
+    const times = 6;
+
+    for (let i = 0; i < times; i++) {
+        businessList.push(
+            <li key={i}>{Business()}</li> 
+        );   
+    };
+
+
+    return (
+        <ul>
+            {businessList}
+        </ul>
+    );
+
+}
+
+export default BusinessList;
