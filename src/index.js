7c94889093373134180ad09513b90609b52ba394
pete <pkeen7@gmail.com>
2023-04-25 13:27:17 -0600

    add Roboto font to project

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CssBaseline } from '@mui/material';
+
+// Imported Fonts - Not sure this is the right place but it said entry point
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>
 );
