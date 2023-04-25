d5d7a0b1a80bc41bed9b2be940a686cbe0b7d11e
pete <pkeen7@gmail.com>
2023-04-24 20:30:25 -0600

    add basic SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,6 +4,36 @@ function SearchBar() {
 
     return (
         <form method="GET">
+            <div id='sort_by'>
+                <input 
+                    type='radio' 
+                    id="best-match"
+                    name="sort_by"
+                    value="best_match">
+                </input>
+                <label for="best-match">
+                    Best Match
+                </label>
+                <input 
+                    type='radio' 
+                    id="rating"
+                    name="sort_by"
+                    value="rating">
+                </input>
+                <label for="rating">
+                    Highest Rated
+                </label>
+                <input 
+                    type='radio' 
+                    id="review-count"
+                    name="sort_by"
+                    value="review_count">
+                </input>
+                <label for="review-count">
+                    Most Reviewed
+                </label>
+            </div>
+            
             <input 
                 type="text"
                 name="term"
