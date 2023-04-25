34ea27cca3ce90e2db4ad59bed356dbf9d32fb24
pete <pkeen7@gmail.com>
2023-04-25 13:59:33 -0600

    Change BusinessUl component to grid system with media queries, previously flex-box style

--- src/components/BusinessList.js
+++ src/components/BusinessList.js
@@ -1,5 +1,30 @@
 import React from 'react';
 import Business from "./Business";
+import styled from 'styled-components';
+
+const BusinessUl = styled.ul`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-rows: 500px;
+    grid-gap: 4rem 50px;
+
+    @media only screen and (max-width: 1250px) {
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+    
+    @media only screen and (max-width: 900px) {
+            grid-template-columns: 1fr 1fr;
+    }
+    
+    @media only screen and (max-width: 700px) {
+            grid-template-columns: 1fr;
+    }
+
+`
+
+const BusinessLi = styled.li`
+    display: inline;
+`
 
 function BusinessList() {
 
@@ -9,15 +34,15 @@ function BusinessList() {
 
     for (let i = 0; i < times; i++) {
         businessList.push(
-            <li key={i}> <Business /> </li> 
+            <BusinessLi key={i}> <Business /> </BusinessLi> 
         );   
     };
 
 
     return (
-        <ul>
+        <BusinessUl>
             {businessList}
-        </ul>
+        </BusinessUl>
     );
 
 }
