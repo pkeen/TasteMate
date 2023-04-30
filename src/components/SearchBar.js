a031fab26b6d336235d1d409c8234d2682bb8735
pete <pkeen7@gmail.com>
2023-04-30 16:01:22 -0600

    Improve styling of searchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,35 +1,96 @@
 import React from 'react';
 import { TextField } from '@mui/material';
-import {Input} from '@mui/material';
 import Button from '@mui/material/Button';
+import { Grid } from '@mui/material';
+import style from "./SearchBar.module.css";
 
 
+
+const heightStyle = {
+    style: {
+        height: 100
+    }
+}
+
+const styles = theme => ({
+    input: {
+      height: 40
+    },
+    button: {
+      height: 40
+    },
+    selectRoot: {
+      height: 40,
+      display: "table"
+      // display: "flex",
+      // justifyContent: "center",
+      // alignItems: "center",
+    },
+    select: {
+      height: 40,
+      paddingTop: 0,
+      paddingBottom: 0,
+      display: "table-cell",
+      verticalAlign: "middle"
+    }
+  });
 
 function SearchBar() {
 
-    return (
-        <form>  
-            <TextField 
-                type="text"
-                autoFocus="true"
-                label="Search"
-                name="search"
-                variant="filled">
-            </TextField>
 
-            <TextField
-                type="text"
-                name="term"
-                label="Location"
-                variant="filled">
-            </TextField>
-            <Button
-                type="submit"
-                variant="contained"
-                size="large"
-                >
-                Search
-            </Button>
+    return (
+        <form>
+
+            <Grid container>
+
+                <Grid xs={12} md={4}>
+                    <TextField 
+                        className={style.inputField}
+                        InputLabelProps={{
+                            shrink: true
+                          }}
+                        type="text"
+                        autoFocus="true"
+                        label="Search"
+                        name="search"
+                        variant="filled"
+                        fullWidth>
+                    </TextField>
+                </Grid>
+
+                <Grid xs={12} md={4}>
+                    <TextField
+                        className={style.inputField}
+                        InputLabelProps={{
+                            shrink: true
+                          }}
+                        fullWidth
+                        type="text"
+                        name="term"
+                        label="Location"
+                        variant="filled">
+                    </TextField>
+                </Grid>
+                        
+                <Grid xs={12} md={4}>
+                    <Button
+                        className={style.inputField}
+                        fullWidth
+                        type="submit"
+                        variant="contained"
+                        size="large"
+                        >
+                        Search
+                    </Button>
+                </Grid>
+                    
+              
+            </Grid>
+            
+
+            
+
+            
         </form>
         
     )
