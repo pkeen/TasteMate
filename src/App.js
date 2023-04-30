ad433d15984c03a9f975b06260386fc4d62f62e1
pete <pkeen7@gmail.com>
2023-04-30 12:41:28 -0600

    add custom theme MUI

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import React from 'react';
 import BusinessList from './components/BusinessList';
 import SearchBar from './components/SearchBar';
 import Logo from './components/logo';
+import { ThemeProvider, createTheme  } from '@mui/material';
+import { green, orange } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: orange,
+    secondary: green,
+  }
+})
 
 function App() {
   return (
     <div className="App">
-      <Logo />
-      <SearchBar />
-      <BusinessList />
+      <ThemeProvider theme={theme}>
+        <Logo />
+        <SearchBar />
+        <BusinessList />
+      </ThemeProvider>
     </div>
 
   );
