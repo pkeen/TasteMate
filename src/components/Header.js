85b0cb77430abb4eee93cf8b5c11142db91c857f
pete <pkeen7@gmail.com>
2023-04-30 16:44:07 -0600

    add grid styling to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Title from "./Title";
 import SearchBar from "./SearchBar";
-import { Box } from "@mui/material";
+import { Grid } from '@mui/material';
+/** @jsx jsx */
+import { jsx } from '@emotion/react';
+import { css } from '@emotion/react'
+
+
 
 function Header() {
     return (
-        <Box >
-            <Title></Title>
-            <SearchBar></SearchBar>
-        </Box>
+        <Grid container css={{marginBottom: '1rem'}}>
+            
+            <Grid xs={12}>
+                <Title></Title>
+            </Grid>
+
+            <Grid xs={12}>
+                <SearchBar></SearchBar>
+            </Grid>
+            
+        </Grid>
     )
 }
 
