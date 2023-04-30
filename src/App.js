2777279b847e77e2716a348ed5abeaa57b2d14e1
pete <pkeen7@gmail.com>
2023-04-30 16:03:35 -0600

    add responsive sizing for H1 element to theme

--- src/App.js
+++ src/App.js
@@ -8,24 +8,35 @@ import Header from './components/Header';
 import { ThemeProvider, createTheme  } from '@mui/material';
 import { green, orange } from '@mui/material/colors';
 import { CssBaseline } from '@mui/material';
+import {Container} from '@mui/material';
 
 const theme = createTheme({
   palette: {
     mode: 'dark',
     primary: orange,
     secondary: green,
-  }
+  },
 })
+
+theme.typography.h1 = {
+    fontSize: '1.4rem',
+    [theme.breakpoints.up('md')]: {
+      fontSize: '2.4rem',
+    },
+    [theme.breakpoints.up('lg')]: {
+      fontSize: '3.2rem',
+    },
+}
 
 function App() {
   return (
-    <div className="App">
+    <Container className="App">
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Header />
         <BusinessList />
       </ThemeProvider>
-    </div>
+    </Container>
 
   );
 }
